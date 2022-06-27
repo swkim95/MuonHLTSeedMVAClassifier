e63a66a62dab1aa52837811908528f6a91857367
Sungwon Kim <kevinn9876@yonsei.ac.kr>
2022-06-28 04:54:16 +0900

    seed.recHits() fix

--- src/SeedMvaEstimator2.cc
+++ src/SeedMvaEstimator2.cc
@@ -153,9 +153,8 @@ vector< pair<LayerHit, LayerTSOS> > SeedMvaEstimatorPhase2::getHitTsosPairs( con
         vector< pair<LayerHit, LayerTSOS> > hitTsosPair = {};
         int ihit = 0;
         // From CMSSW_12_? ~ 
-        // auto [recHit_begin, recHit_end] = seed.recHits();
-        //for( auto hit = recHit_begin; hit!=recHit_end; ++hit ) {
         for( auto hit = seed.recHits().first; hit!=seed.recHits().second; ++hit ) {
+        //for( const auto hit : seed.recHits() ) {
             // -- look for closest tsos by absolute distance
             // FIXME: this is random choice
             int the_tsos = -99999;
